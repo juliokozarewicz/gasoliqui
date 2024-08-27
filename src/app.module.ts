9fc0d2e163804436508ca63ef8bc206861742664
Julio Kozarewicz <julioerk@outlook.com>
2024-08-27 14:20:12 -0300

    [ UPLOAD ] ( gemini ) Conexão com o gemini e retorno se resultado iniciado.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { CoreModule } from './modules/core/core.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+
+    // configs
+    ConfigModule.forRoot(),
 
     // my modules
     CoreModule,
 
     // database
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'database.sqlite',
+      type: process.env.DB_TYPE,
+      database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     } as TypeOrmModuleOptions)
--- src/modules/core/core.service.ts
+++ src/modules/core/core.service.ts
@@ -5,15 +5,24 @@ import {
 } from "@nestjs/common"
 import { ReadDataEntity } from "./core.entity"
 import { InjectRepository } from "@nestjs/typeorm"
-import { Repository } from "typeorm"
+import { IntegerType, Repository } from "typeorm"
 import { ReadDataExtendedDTO } from "./core.dto"
 import { logsGenerator } from "app.logs"
 
+import { GoogleAIFileManager } from "@google/generative-ai/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+
+
 // Define the interface for the response
 export interface standardResponse {
+    data: any,
     statusCode: number,
     message: string,
+    measure_value: IntegerType,
+    measure_uuid: string,
     _links: {
+        image_url: { href: string },
         self: { href: string },
         next: { href: string },
         prev: { href: string }
@@ -46,12 +55,47 @@ export class ReadDataService {
 
         try {
 
-            console.log(readDataExtendedDTO)
+            // upload image
+            // --------------------------------------------------------------------------
+            const fileManager = new GoogleAIFileManager(process.env.GEMINI_API_KEY);            
+
+            const uploadResponse = await fileManager.uploadFile("./src/1_media/luz.jpg", {
+                mimeType: "image/jpeg",
+                displayName: "Jetpack drawing",
+            });
+            // --------------------------------------------------------------------------
+
+            // process image #####
+            // --------------------------------------------------------------------------
+
+            const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+            const model = genAI.getGenerativeModel({
+                model: "gemini-1.5-pro",
+            });
+
+            const result = await model.generateContent([
+                {
+                  fileData: {
+                    mimeType: uploadResponse.file.mimeType,
+                    fileUri: uploadResponse.file.uri
+                  }
+                },
+                {
+                    text: "Get the numerical (only numbers) value for measuring water, gas, electricity. " +
+                    "Make a formatted and simple answer, without text e.g. measurement: 123456 (only numbers)"
+                },
+            ]);
+            // -------------------------------------------------------------------------
 
             return {
-                statusCode: 201,
-                message: "*** teste ***",
+                data: result,
+                statusCode: 200,
+                message: 'leitura realizada com sucesso',
+                measure_value: parseInt(result.response.candidates[0].content.parts[0].text),
+                measure_uuid: 'UUID string',
                 _links: {
+                    image_url: { href: "/api/upload" },
                     self: { href: "/api/upload" },
                     next: { href: `/api/confirm`},
                     prev: { href: "/api/{customer code}/list" }
